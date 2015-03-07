1d1a01ffb8b559058e848c8b76efa1f9501f7427
cwz.scardigno@gmail.com <TiAmoRaff@47>
2015-03-07 13:16:41 +0100

    Add possibility to refresh menu when you add new section|devider|label and when you would with refreshMenu() method

--- example/src/main/java/de/madcyph3r/example/FragmentIndex.java
+++ example/src/main/java/de/madcyph3r/example/FragmentIndex.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import de.madcyph3r.materialnavigationdrawer.MaterialNavigationDrawer;
+
 /**
  * Created by neokree on 24/11/14.
  */
@@ -18,8 +20,17 @@ public class FragmentIndex extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         TextView text = new TextView(this.getActivity());
-        text.setText("Section");
+        text.setText("Section - Click me to add new section dinamically");
         text.setGravity(Gravity.CENTER);
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int i = ((MaterialNavigationDrawer)getActivity()).getCurrentMenu().getSections().size();
+                ((MaterialNavigationDrawer)getActivity()).newSection("new section " + i, getResources().getDrawable(R.drawable.ic_launcher), false, ((MaterialNavigationDrawer)getActivity()).getCurrentMenu(), true);
+            }
+        });
+
         return text;
 
     }
